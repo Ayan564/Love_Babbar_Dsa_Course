81aa2fc6e518ccf142e011590587dc3bde1a8c5f
Ayan564 <ayanmaity564@gmail>
2025-02-14 09:19:52 +0530

    queue and assignment pdf add

--- Queues/Queue Class - 2/reverseInKGroup.cpp
+++ Queues/Queue Class - 2/reverseInKGroup.cpp
@@ -3,12 +3,28 @@
 #include<stack>
 using namespace std;
 
-void reverseInKGroup(queue<int> &q, int k){
-    int n = q.size();
-    if(n < k){
-        return;
+void reverseInKGroup(queue<int> &q, int k, int count){
+    if(count >= k){
+        stack<int> s;
+        // fetch the first k elements from the queue and push them into the stack
+        for(int i = 0; i < k; i++){
+            s.push(q.front());
+            q.pop();
+        }
+        // pop the elements from the stack and push them back into the queue
+        while(!s.empty()){
+            q.push(s.top());
+            s.pop();
+        }
+        reverseInKGroup(q, k, count - k);
     }
-    
+    else{
+        // if the remaining elements are less than k, then push them back into the queue
+        for(int i = 0; i < count; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
 }
 
 int main(){
@@ -26,7 +42,8 @@ int main(){
     q.push(110);
 
     int k = 3;
-    reverseInKGroup(q, k);
+    int count = q.size(); // it tracks the size of the queue
+    reverseInKGroup(q, k, count);
     while(!q.empty()){
         cout<<q.front()<<" ";
         q.pop();
